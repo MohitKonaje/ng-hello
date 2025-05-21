eda1b5a9d9af29e1f717672c3242a21e1b797d08
mohit <mohit.konaje@gmail.com>
2025-05-21 13:59:27 +0530

    base routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import {ListComponent} from './list/list.component';
+import {HomeComponent} from './home/home.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'list', component: ListComponent },
+    { path: 'list/:id', component: ListComponent },
+    { path: '**', 
+       redirectTo: (route)=>{return '/home'}, 
+    //    component: HomeComponent 
+    },
+
+];
